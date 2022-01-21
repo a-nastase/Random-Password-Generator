7e68452339ec4eac3be753906e4f9101389c670b
alexnasty6 <86931667+alexnasty6@users.noreply.github.com>
2022-01-21 11:01:02 +0200

    Updated the code (new feature -- select number of characters)

--- App.java
+++ App.java
@@ -9,6 +9,8 @@ public class App extends JFrame implements MouseListener {
 
     private static JTextField Password = new JTextField("Click GO to generate new password (check clipboard)", SwingConstants.CENTER);
     static JButton go = new JButton("GO!");
+    static JTextField NrChars = new JTextField("10", SwingConstants.CENTER);
+    static JLabel C = new JLabel("Please enter a valid number of characters:", SwingConstants.CENTER);
 
     JLabel nothing = new JLabel();
 
@@ -21,7 +23,19 @@ public class App extends JFrame implements MouseListener {
         Password.setOpaque(true);
         Password.setVisible(true);
 
-        go.setBounds((int) 117.5, 80, 185, 50);
+        C.setBounds(60, 70, 300, 20);
+        C.setOpaque(true);
+        C.setVisible(true);
+
+        NrChars.setBounds(60, 100, 300, 50);
+        NrChars.setEditable(true);
+        NrChars.setForeground(Color.black);
+        NrChars.setBackground(Color.white);
+        NrChars.setFont(new Font("DialogInput", Font.BOLD, (int) 9.8));
+        NrChars.setOpaque(true);
+        NrChars.setVisible(true);
+
+        go.setBounds((int) 117.5, 160, 185, 50);
         go.setOpaque(true);
         go.setVisible(true);
         go.addMouseListener(this);
@@ -34,6 +48,8 @@ public class App extends JFrame implements MouseListener {
 
         this.add(go);
         this.add(Password);
+        this.add(C);
+        this.add(NrChars);
         this.add(nothing);
         this.setVisible(true);
     }
@@ -43,15 +59,15 @@ public class App extends JFrame implements MouseListener {
         if(e.getSource() == go) {
             String RandPass = "";
             Random r = new Random();
-
-            String fn = "45";
-            int Char1 = fn.charAt(r.nextInt(fn.length()));
-            String sn = "1234567890";
-            int Char2 = sn.charAt(r.nextInt(sn.length()));
-            int chars = (Char1 * 10) + Char2;
+            int chars = 10;
+            try {
+                chars = Integer.parseInt(NrChars.getText());
+            } catch(Error err) {
+                chars = 10;
+            }
 
             String alphabet = "123456789abcdefghijklmnopqrstuvwxyz`/*-+.,<>?_=)(&^%$#@!";
-            for (int i = 0; i < Math.random() * chars; i++) {
+            for (int i = 0; i < chars; i++) {
                 RandPass = alphabet.charAt(r.nextInt(alphabet.length())) + RandPass;
             }
 
